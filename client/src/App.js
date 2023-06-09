4c8dd4219332a4bff2e5ddcba148070f3d02fa8f
Hedi Sfaxi <hedi.sfaxi02@gmail.com>
2023-06-09 16:20:29 +0200

    premiers tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,52 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import SimpleContainer from './components/containers/FluidContainer';
+import BasicStack from './components/BasicStack';
+import ButtonAppBar from './components/AppBar';
+import BoxSx from './components/containers/Box';
+
 
 export default function App() {
 
-  // data comes from backend
+  // data 
   const [data, setData] = React.useState(null);
+  const [age, setAge] = React.useState('');
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
-  // 
+
+  // fonctions
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   return (
     <div>
-      <Button variant="contained">{!data ? "Loading..." : data}</Button>
+      <ButtonAppBar></ButtonAppBar>
+      <BasicStack></BasicStack>
     </div>
   );
 }
