a7d13e5afbe4ccf673dbd5c545b60d049428c29c
augusto-moraes <moraestf.augusto@gmail.com>
2023-06-09 14:30:52 +0200

    code organisation

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; 
+
+import './App.css';
+
+import ExempleMUI from './components/Exemple/ExempleMUI';
+import NavBar from "./components/common/NavBar";
 
 export default function App() {
-
-  // data comes from backend
-  const [data, setData] = React.useState(null);
-
-  // 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div>
-      <Button variant="contained">{!data ? "Loading..." : data}</Button>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' component={<ExempleMUI/>} />
+          </Routes>
+        </div>
+      
+      </div>   
+    </Router>
   );
 }
