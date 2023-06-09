ec6be7f7b36fdb6e4b0967a8a2c4a47a1c0a11e6
augusto-moraes <moraestf.augusto@gmail.com>
2023-06-09 11:23:02 +0200

    add MUI template

---
+++ client/src/ExempleMUI.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ExempleMUI() {
+
+  // data comes from backend
+  const [data, setData] = React.useState(null);
+
+  // fetches data from backend (port 3001) and saves it in session as "data", to be displayed in the button
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div>
+      <Button variant="contained">{!data ? "Loading..." : data}</Button>
+    </div>
+  );
+}
