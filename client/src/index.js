3728b67fa7249e566e1b75060df81cbeff12bc34
augusto-moraes <moraestf.augusto@gmail.com>
2023-06-09 15:52:24 +0200

    routes done :)

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,10 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import './index.css';
-import App from './App';
+import Root from "./routes";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
+  { path: "*", Component: Root },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ client/src/routes.js
@@ -0,0 +1,51 @@
+import {
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import './routes.css';
+
+import NavBar from "./components/common/NavBar";
+
+export default function Root() {
+  return (
+    <div className="App">
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog/*" element={<BlogApp />} />
+            <Route path="/users/*" element={<UserApp />} />
+        </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <p>
+        Check out the <Link to="/blog">blog</Link> or the{" "}
+        <Link to="users">users</Link> section
+      </p>
+    </>
+  );
+}
+
+function BlogApp() {
+  return (
+    <Routes>
+      <Route index element={<h1>Blog Index</h1>} />
+      <Route path="posts" element={<h1>Blog Posts</h1>} />
+    </Routes>
+  );
+}
+
+function UserApp() {
+  return (
+    <Routes>
+      <Route index element={<h1>Users Index</h1>} />
+    </Routes>
+  );
+}
