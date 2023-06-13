183bb5d14b822df169792ff90a76fb4c9841a81b
Mounir <mounirradj.km@gmail.com>
2023-06-13 13:30:37 +0200

    selection pannels modification

--- client/src/components/reservation/AutoGrid.js
+++ client/src/components/reservation/AutoGrid.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import BasicSelect from '../common/Select';
+import { Button } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,26 +15,24 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function AutoGrid() {
-  const options1 = ['Option A', 'Option B', 'Option C'];
-  const options2 = ['Option 1', 'Option 2', 'Option 3'];
-  const salles = ['TDC', 'TDD', 'TDE'];
-  const team = ['Augusto', 'Mounir', 'Hedi', 'Kowsi', 'Matthieu'];
+  const salles = ['TD A', 'TD B', 'TD C', 'TD D', 'TD E', 'TD F', 'TP A', 'TP B', 'TP C', 'TP D', 'TP E', 'Projet A', 'Projet B'];
+  const heures = ['8h', '8h30', '9h', '9h30', '10h', '10h30', '11h', '11h30', '12h', '12h30', '13h', '13h30', '14h', '14h30', '15h', '15h30', '16h', '16h30', '17h', '17h30', '18h', '18h30', '19h', '19h30', '20h', '20h30', '21h', '21h30', '22h', '22h30'];
+  const durees = ['30min', '1h', '1h30', '2h'];
+  //const team = ['Augusto', 'Mounir', 'Hedi', 'Kowsi', 'Matthieu'];
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={4}>
         <Grid item xs>
-          <Item><BasicSelect options={options1} placeholder='ABC'/></Item>
+          <Item><BasicSelect options={salles} placeholder='Salle'/></Item>
         </Grid>
         <Grid item xs>
-          <Item><BasicSelect options={options2} placeholder='123'/></Item>
+          <Item><BasicSelect options={heures} placeholder='Heure'/></Item>
         </Grid>
         <Grid item xs>
-          <Item><BasicSelect options={salles} placeholder='Salles'/></Item>
-        </Grid>
-        <Grid item xs>
-          <Item><BasicSelect options={team} placeholder='Team'/></Item>
-        </Grid>
+          <Item><BasicSelect options={durees} placeholder='Duree'/></Item>
+        </Grid>   
+        <Button> Valider </Button>
       </Grid>
     </Box>
   );
